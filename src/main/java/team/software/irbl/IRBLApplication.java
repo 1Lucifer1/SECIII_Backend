df0be6b3be428657c58c51295a603e06c04282a1
1Lucifer1 <2281854129@qq.com>
2021-03-06 14:54:04 +0800

    init

--- src/main/java/team/software/irbl/IRBLApplication.java
+++ src/main/java/team/software/irbl/IRBLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IrblApplication {
+public class IRBLApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IrblApplication.class, args);
+        SpringApplication.run(IRBLApplication.class, args);
     }
 
 }
