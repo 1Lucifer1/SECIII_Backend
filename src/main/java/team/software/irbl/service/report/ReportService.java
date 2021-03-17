1eed110170dcd6e2d5575b71c4fadd62db3a414a
Whisky <2324818984@qq.com>
2021-03-17 18:29:35 +0800

    style: 补充service下各方法的使用注释

--- src/main/java/team/software/irbl/service/report/ReportService.java
+++ src/main/java/team/software/irbl/service/report/ReportService.java
@@ -6,5 +6,11 @@ import team.software.irbl.util.Err;
 import java.util.List;
 
 public interface ReportService {
+    /**
+     * 读取指定项目下的所有缺陷报告列表
+     * @param projectIndex
+     * @return
+     * @throws Err
+     */
     public List<Report> getAllReportsByProjectIndex(Integer projectIndex) throws Err;
 }
