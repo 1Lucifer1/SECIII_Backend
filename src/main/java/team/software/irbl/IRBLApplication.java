80563908e8d4d863c8820daa65086f864a438ff6
Snowding Zerland <181250011@smail.nju.edu.cn>
2021-03-20 15:58:37 +0800

    feat: 使用继承commandline来实现初始化化时调用方法

--- src/main/java/team/software/irbl/IRBLApplication.java
+++ src/main/java/team/software/irbl/IRBLApplication.java
@@ -1,15 +1,25 @@
 package team.software.irbl;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import team.software.irbl.core.VSM;
 
 @MapperScan("team.software.irbl.mapper")
 @SpringBootApplication
-public class IRBLApplication {
+public class IRBLApplication implements CommandLineRunner {
+
+    @Autowired
+    private VSM vsm;
 
     public static void main(String[] args) {
         SpringApplication.run(IRBLApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        vsm.startLocalRank();
+    }
 }
