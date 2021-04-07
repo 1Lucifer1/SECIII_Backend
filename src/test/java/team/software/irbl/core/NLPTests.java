832f57db57afc1732a922de1ae3694417420ea63
Whisky <2324818984@qq.com>
2021-04-08 01:03:30 +0800

    feat: NLP

---
+++ src/test/java/team/software/irbl/core/NLPTests.java
@@ -0,0 +1,22 @@
+package team.software.irbl.core;
+
+import org.junit.Test;
+import team.software.irbl.core.nlp.NLP;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class NLPTests {
+
+    //sample text for tests
+    private static final String example = "/**The China_history of coreOfNLPMethods generally starts in the 1950s, although ChineseHistories can be found from earlier periods.*/";
+
+    @Test
+    public void standfordNLPTest(){
+        List<String> wordList = NLP.standfordNLP(example);
+        for (String word:wordList) {
+            System.out.println(word);
+        }
+    }
+}
