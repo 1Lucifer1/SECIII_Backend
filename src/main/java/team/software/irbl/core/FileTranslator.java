4130b00931f7fecb777f03849147be06b879032d
fengxinze <181250031@smail.nju.edu.cn>
2021-04-14 12:05:48 +0800

    fix: final

--- src/main/java/team/software/irbl/core/FileTranslator.java
+++ src/main/java/team/software/irbl/core/FileTranslator.java
@@ -7,11 +7,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import team.software.irbl.core.domain.StructuredCodeFile;
+import team.software.irbl.domain.BugReport;
 import team.software.irbl.util.SavePath;
 
 import java.io.*;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,39 +24,74 @@ public class FileTranslator {
         File file =new File(path);
         String content="";
         if(file.exists()&&file.length()!=0){
-            InputStream input=new FileInputStream(file);
-            byte[] data=new byte[1024];
-            int len=input.read(data);
-            //System.out.println(len);
-            content = new String(data,0,len);
-            //System.out.println(content);
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(path));
+                String contentLine;
+                while ((contentLine = br.readLine()) != null) {
+                    System.out.println(contentLine);
+                    content = content+contentLine;
+                }
+                //输出数组
+                //System.out.println(content);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error in closing the BufferedReader");
+                }
+            }
         }
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(obj);
         //mapper.writeValue(new File("/Users/fengxinze/Desktop/IRBL/json/src/main/java/a.txt"),json);
         System.out.println(json);
         FileWriter writer = new FileWriter(path);
-        writer.write(content+json+" ");
+        writer.write(content+json+"$");
         writer.close();
+        //writer.close();
     }
     public  void writeCodeFile(StructuredCodeFile obj) throws IOException {
         String path = SavePath.getFilePath()+"codeFile.txt";
         File file =new File(path);
         String content="";
         if(file.exists()&&file.length()!=0){
-            InputStream input=new FileInputStream(file);
-            byte[] data=new byte[1024];
-            int len=input.read(data);
-            //System.out.println(data);
-            content = new String(data,0,len);
-            //System.out.println(content);
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(path));
+                String contentLine;
+                while ((contentLine = br.readLine()) != null) {
+                    System.out.println(contentLine);
+                    content = content+contentLine;
+                }
+                //输出数组
+                //System.out.println(content);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error in closing the BufferedReader");
+                }
+            }
         }
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(obj);
         //mapper.writeValue(new File("/Users/fengxinze/Desktop/IRBL/json/src/main/java/a.txt"),json);
         //System.out.println(json);
         FileWriter writer = new FileWriter(path);
-        writer.write(content+json+" ");
+        writer.write(content+json+"$");
         writer.close();
     }
     public static List<StructuredBugReport> readBugReport() throws IOException {
@@ -61,21 +99,39 @@ public class FileTranslator {
         File file =new File(path);
         String content="";
         if(file.exists()&&file.length()!=0){
-            InputStream input=new FileInputStream(file);
-            byte[] data=new byte[1024];
-            int len=input.read(data);
-            //System.out.println(data);
-            content = new String(data,0,len);
-            System.out.println(content);
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(path));
+                String contentLine;
+                while ((contentLine = br.readLine()) != null) {
+                    System.out.println(contentLine);
+                    content = content+contentLine;
+                }
+                //输出数组
+                //System.out.println(content);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error in closing the BufferedReader");
+                }
+            }
         }
-        String[] jsons = content.split(" ");
+        String[] jsons = content.split("$");
         int l = jsons.length;
         List<StructuredBugReport> res = new ArrayList<>();
         //StructuredBugReport[] res = new StructuredBugReport[l];
         for(int i=0;i<l;i++){
+            System.out.println(jsons[i]);
             ObjectMapper mapper = new ObjectMapper();
-            StructuredBugReport br = mapper.readValue(jsons[i], StructuredBugReport.class);
-            res.add(br);
+            StructuredBugReport sbr = mapper.readValue(jsons[i], StructuredBugReport.class);
+            res.add(sbr);
         }
         return res;
     }
@@ -84,14 +140,31 @@ public class FileTranslator {
         File file =new File(path);
         String content="";
         if(file.exists()&&file.length()!=0){
-            InputStream input=new FileInputStream(file);
-            byte[] data=new byte[2048];
-            int len=input.read(data);
-            //System.out.println(data);
-            content = new String(data,0,len);
-            //System.out.println(content);
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(path));
+                String contentLine;
+                while ((contentLine = br.readLine()) != null) {
+                    System.out.println(contentLine);
+                    content = content+contentLine;
+                }
+                //输出数组
+                //System.out.println(content);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error in closing the BufferedReader");
+                }
+            }
         }
-        String[] jsons = content.split(" ");
+        String[] jsons = content.split("$");
         int l = jsons.length;
         List<StructuredCodeFile> res = new ArrayList<>();
         for(int i=0;i<l;i++){
