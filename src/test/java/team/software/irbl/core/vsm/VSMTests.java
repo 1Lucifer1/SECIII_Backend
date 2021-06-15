3b9f0154ac748f18d3264d2e26ed60b0028b0980
1Lucifer1 <2281854129@qq.com>
2021-06-15 12:56:34 +0800

    feat: reporter component test

--- src/test/java/team/software/irbl/core/vsm/VSMTests.java
+++ src/test/java/team/software/irbl/core/vsm/VSMTests.java
@@ -2,9 +2,21 @@ package team.software.irbl.core.vsm;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import team.software.irbl.core.IndicatorEvaluation;
+import team.software.irbl.core.dbstore.DBProcessor;
+import team.software.irbl.core.dbstore.DBProcessorFake;
 import team.software.irbl.core.domain.StructuredBugReport;
 import team.software.irbl.core.domain.StructuredCodeFile;
+import team.software.irbl.core.filestore.XMLParser;
+import team.software.irbl.core.jdt.JavaParser;
+import team.software.irbl.core.maptool.CodeFileMap;
+import team.software.irbl.core.maptool.FilePathMap;
+import team.software.irbl.core.maptool.PackageMap;
+import team.software.irbl.core.reporterComponent.ReporterRank;
 import team.software.irbl.domain.BugReport;
+import team.software.irbl.domain.RankRecord;
+import team.software.irbl.dto.project.Indicator;
+import team.software.irbl.util.SavePath;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,5 +48,34 @@ public class VSMTests {
 //                Assert.assertEquals(rankRecord.getCosineSimilarity(), 1.0, 1);
 //            });
 //        }
+    }
+
+    @Test
+    public void testReporterComponent(){
+        String projectName = "swt-3.1";
+
+        List<BugReport> reports = XMLParser.getBugReportsFromXML(SavePath.getSourcePath(projectName) + "/bugRepository.xml", 1);
+        List<StructuredCodeFile> codeFiles = JavaParser.parseCodeFilesInDir(SavePath.getSourcePath(projectName + "/"), 1);
+        DBProcessor dbProcessor = new DBProcessorFake();
+        dbProcessor.saveCodeFiles(new ArrayList<>(codeFiles));
+        CodeFileMap codeFileMap = new PackageMap(new ArrayList<>(codeFiles));
+//        dbProcessor.saveBugReports(reports, codeFileMap);
+        dbProcessor.saveBugReports(reports, new FilePathMap(new ArrayList<>(codeFiles)));
+
+        ReporterRank reporterRank = new ReporterRank(projectName, codeFiles);
+
+        assert reports != null;
+        reports.forEach(report -> {
+            List<RankRecord> records = reporterRank.rank(report);
+            report.setRanks(records);
+        });
+
+        IndicatorEvaluation indicatorEvaluation = new IndicatorEvaluation();
+        Indicator indicator = indicatorEvaluation.getEvaluationIndicator(reports);
+        Assert.assertTrue(indicator.getTop1() > 0.0);
+        Assert.assertTrue(indicator.getTop5() > 0.0);
+        Assert.assertTrue(indicator.getTop10() > 0.0);
+        Assert.assertTrue(indicator.getMAP() > 0.0);
+        Assert.assertTrue(indicator.getMRR() > 0.0);
     }
 }
