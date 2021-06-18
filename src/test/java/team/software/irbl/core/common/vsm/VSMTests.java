335465d83a6e5b850935d15afa262e8a72238455
Whisky <2324818984@qq.com>
2021-06-18 17:31:17 +0800

    fix: VSMTests

--- src/test/java/team/software/irbl/core/common/vsm/VSMTests.java
+++ src/test/java/team/software/irbl/core/common/vsm/VSMTests.java
@@ -32,33 +32,6 @@ import java.util.List;
 public class VSMTests {
 
     @Test
-    public void testRank(){
-//        VSM vsm = new VSM();
-//        java.util.List<StructuredBugReport> reportList = new ArrayList<>();
-//        List<String> test = new ArrayList<>();
-//        test.add("test");
-//        StructuredBugReport report = new StructuredBugReport(new BugReport(0,  0,  "1", "1", "test", "test", new ArrayList<>()));
-//        report.setDescriptionWords(test);
-//        report.setSummaryWords(test);
-//        reportList.add(report);
-//        List<StructuredCodeFile > codeFileList = new ArrayList<>();
-//        StructuredCodeFile sc = new StructuredCodeFile();
-//        sc.setComments(test);
-//        sc.setContexts(test);
-//        sc.setFields(test);
-//        sc.setMethods(test);
-//        sc.setTypes(test);
-//        codeFileList.add(sc);
-//        vsm.startRank(reportList, codeFileList);
-//        for (StructuredBugReport structuredBugReport : reportList) {
-//            structuredBugReport.getRanks().forEach(rankRecord->{
-//                System.out.println(rankRecord.getFileIndex() + ": " + rankRecord.getCosineSimilarity());
-//                Assert.assertEquals(rankRecord.getCosineSimilarity(), 1.0, 1);
-//            });
-//        }
-    }
-
-    @Test
     public void testReporterComponent(){
         String projectName = "swt-3.1";
 
